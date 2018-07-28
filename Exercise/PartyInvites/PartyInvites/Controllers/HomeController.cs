1a44c764e4017bbd3e3ef3292bd62194c4eedfd3
Krishneil <Krishneil1@gmail.com>
2018-07-28 16:19:25 +1000

    Chapter 1 Completed

---
+++ Exercise/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult  Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternnoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //ToDo: store response from guest
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+
+            return View("Thanks",guestResponse);
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(guest => guest.WillAttend == true));
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
